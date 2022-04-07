726b1d01b361aea5c1f1352f8d158a50d35b53d4
Aaryan Raj Sarda <aaryanrajsarda@gmail.com>
2022-04-07 11:57:03 +0530

    Updated Form

--- src/Profile.js
+++ src/Profile.js
@@ -4,19 +4,9 @@ import Report from "./Report";
 import { useStateValue } from "./StateProvider";
 import EditIcon from "@material-ui/icons/Edit";
 import { makeStyles } from "@material-ui/core/styles";
-import { Avatar, InputLabel, Modal, Typography } from "@material-ui/core";
-import Popover from "@material-ui/core/Popover";
-import { Button, Tooltip } from "@material-ui/core";
+import { Modal, Button } from "@material-ui/core";
 import { db } from "./firebase";
-import {
-  onSnapshot,
-  query,
-  where,
-  collection,
-  collectionGroup,
-  doc,
-  setDoc,
-} from "firebase/firestore";
+import { onSnapshot, collection, doc, setDoc } from "firebase/firestore";
 
 function rand() {
   return Math.round(Math.random() * 20) - 10;
@@ -65,32 +55,27 @@ const useModalStyles = makeStyles((theme) => ({
 }));
 
 function Profile() {
-  const classes = useStyles();
   const modalClasses = useModalStyles();
   const [modalStyle] = React.useState(getModalStyle);
   const [{ user }] = useStateValue();
   const [openModal, setOpenModal] = React.useState(false);
   const [age, setAge] = React.useState(0);
-  const [blood, setBlood] = React.useState(0);
-  const [sex, setSex] = React.useState(0);
+  const [blood, setBlood] = React.useState("");
+  const [sex, setSex] = React.useState("");
   const [mobile, setMobile] = React.useState(0);
   const [details, setDetails] = React.useState(null);
   const [reports, setReports] = React.useState([]);
 
   useEffect(() => {
-    const unsub = onSnapshot(doc(db, "users", user?.uid), (doc) => {
+    onSnapshot(doc(db, "users", user?.uid), (doc) => {
       setDetails(doc.data());
     });
-    const path = `users/${user?.uid}/reports`
-    const d = onSnapshot(
-      collection(db, path),
-      (snap) => {
-        console.log(doc);
-      }
-    );
-
+    const path = `users/${user?.uid}/reports`;
+    onSnapshot(collection(db, path), () => {
+      console.log(doc);
+    });
     const ref = `users/${user?.uid}/reports`;
-    const unsubscribe = onSnapshot(
+    onSnapshot(
       collection(db, ref),
       (snapshot) => {
         snapshot.docs.forEach((doc) => {
@@ -134,7 +119,7 @@ function Profile() {
     <div style={modalStyle} className={modalClasses.paper}>
       <h2>Update Details</h2>
       <form>
-        <div className="row">
+        <div className="row p-2">
           <label className="col-4" for="age">
             Age :
           </label>
@@ -147,31 +132,44 @@ function Profile() {
             onChange={(e) => setAge(e.target.value)}
           ></input>
         </div>
-        <div className="row">
+        <div className="row p-2">
           <label className="col-4" for="blood">
             Blood Group :
           </label>
-          <input
+          <select
             className="col-6 profile__update"
-            name="blood"
             id="blood"
+            name="blood"
             value={blood}
             onChange={(e) => setBlood(e.target.value)}
-          ></input>
+          >
+            <option value="A+">A+</option>
+            <option value="A-">A-</option>
+            <option value="B+">B+</option>
+            <option value="B-">B-</option>
+            <option value="AB+">AB+</option>
+            <option value="AB-">AB-</option>
+            <option value="O+">O+</option>
+            <option value="O-">O-</option>
+          </select>
         </div>
-        <div className="row">
+        <div className="row p-2">
           <label className="col-4" for="sex">
             Gender :
           </label>
-          <input
+          <select
             className="col-6 profile__update"
-            name="sex"
             id="sex"
+            name="sex"
             value={sex}
             onChange={(e) => setSex(e.target.value)}
-          ></input>
+          >
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Others">Others</option>
+          </select>
         </div>
-        <div className="row">
+        <div className="row p-2">
           <label className="col-4" for="mobile">
             Mobile Number :
           </label>
